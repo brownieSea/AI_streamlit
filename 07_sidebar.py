5ebd7010d8096fd4b0131f843db58f1233519769
brownieSea <165341873+brownieSea@users.noreply.github.com>
2024-06-13 15:20:28 +0900

    Add files via upload

---
+++ 07_sidebar.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+
+# 사이드바 화면 (화면 분할)
+st.sidebar.title("SideBar")
+st.sidebar.subheader("login")
+userID = st.sidebar.text_input("ID : ", value="streamlit", max_chars=15)
+userPW = st.sidebar.text_input("PW : ", value="12345", max_chars=15, type='password')
+
+st.sidebar.subheader("SelectBox")
+sel_opt = ["진주 귀걸이를 한 소녀", '별이 빛나는 밤에', '절규', '월하정인']
+user_opt = st.sidebar.selectbox("Favorite Picture : ", sel_opt)
+st.sidebar.write("Choosed : ", user_opt)
+
+# 메인화면
+st.title("스트림릿의 사이드바")
+folder = 'data/'
+image_files = ['Vermeer.png', 'Gogh.png', 'Munch.png', 'ShinYoonbok.png']
+
+sel_img_index = sel_opt.index(user_opt)
+#선택한 항목에 맞는 이미지 파일
+img_file = image_files[sel_img_index]
+img_local = Image.open(f'{folder}{img_file}')
+st.image(img_local, caption=user_opt)
